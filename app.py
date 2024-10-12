643ea445d5a54a481ae52cd5e0a6b9edd1c42a70
moises <moises.p.student@gmail.com>
2024-10-11 21:20:39 -0400

    update

--- app.py
+++ app.py
@@ -20,7 +20,7 @@ app = Flask(__name__)
 app.config['MAIL_SERVER'] = 'smtp.gmail.com'
 app.config['MAIL_PORT'] = 587
 app.config['MAIL_USERNAME'] = 'moises.p.student@gmail.com'
-app.config['MAIL_PASSWORD'] = 'qyfw evcq fxwg eqpw' 
+app.config['MAIL_PASSWORD'] = 'password_app' 
 app.config['MAIL_USE_TLS'] = True
 app.config['MAIL_USE_SSL'] = False
 
